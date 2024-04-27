e8268778cfe1b5b30eaa9577ce726f131e211442
shadowabi <50265741+shadowabi@users.noreply.github.com>
2024-04-27 16:52:21 +0800

    fix print bug

--- cmd/root.go
+++ cmd/root.go
@@ -42,6 +42,7 @@ var logLevel string
 func init() {
 	RootCmd.PersistentFlags().StringVar(&logLevel, "logLevel", "info", "设置日志等级 (Set log level) [trace|debug|info|warn|error|fatal|panic]")
 	RootCmd.CompletionOptions.DisableDefaultCmd = true
+	RootCmd.SetHelpFunc(customHelpFunc)
 	RootCmd.PersistentFlags().StringVarP(&define.File, "file", "f", "", "从文件中读取目标地址 (Input FILENAME)")
 	RootCmd.PersistentFlags().StringVarP(&define.Url, "url", "u", "", "输入目标地址 (Input [ip|domain|url])")
 	RootCmd.PersistentFlags().IntVarP(&define.TimeOut, "timeout", "t", 15, "输入每个 http 请求的超时时间 (Enter the timeout period for every http request)")
@@ -61,4 +62,8 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func customHelpFunc(cmd *cobra.Command, args []string) {
+	cmd.Usage()
 }
